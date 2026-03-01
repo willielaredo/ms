22228c3fd1c53922cc07a35a0d97674eb2b143de
Willie Thompson <willielaredo@gmail.com>
2026-03-01 13:38:00 -0500

    update ALLOWED_HOSTS

--- ms_django/ms/settings.py
+++ ms_django/ms/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['willielaredo.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
